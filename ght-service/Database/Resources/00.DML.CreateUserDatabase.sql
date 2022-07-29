854cee8e42ff524252e7a531ee1804509d7c4b12
Mathew Casper <mathew.casper@casperinc.dev>
2022-07-29 01:07:40 -0400

    fix colation

--- ght-service/Database/Resources/00.DML.CreateUserDatabase.sql
+++ ght-service/Database/Resources/00.DML.CreateUserDatabase.sql
@@ -2,8 +2,8 @@ CREATE DATABASE "ght-db-nprd"
     WITH
     OWNER = "ght-db-test"
     ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF-8'
-    LC_CTYPE = 'en_US.UTF-8'
+    LC_COLLATE = 'en_US.utf-8'
+    LC_CTYPE = 'en_US.utf-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
