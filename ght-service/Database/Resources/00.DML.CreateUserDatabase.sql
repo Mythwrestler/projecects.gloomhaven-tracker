f36f6fca63f0918d58f83ed8e98d52aae217f2b8
Mathew Casper <mathew.casper@casperinc.dev>
2022-07-29 01:13:10 -0400

    change db to use ght_svc_nprd

--- ght-service/Database/Resources/00.DML.CreateUserDatabase.sql
+++ ght-service/Database/Resources/00.DML.CreateUserDatabase.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE "ght-db-nprd"
     WITH
-    OWNER = "ght-db-test"
+    OWNER = "svc_ght_nprd"
     ENCODING = 'UTF8'
     LC_COLLATE = 'en_US.utf8'
     LC_CTYPE = 'en_US.utf8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
-GRANT ALL ON DATABASE "ght-db-nprd" TO "ght-db-test";
+GRANT ALL ON DATABASE "ght-db-nprd" TO "svc_ght_nprd";
 
-GRANT TEMPORARY, CONNECT ON DATABASE "ght-db-nprd" TO "ght-db-test";
+GRANT TEMPORARY, CONNECT ON DATABASE "ght-db-nprd" TO "svc_ght_nprd";
