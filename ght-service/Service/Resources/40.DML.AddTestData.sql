76e50dc143cec9a8ff0f7751acaef03d05ef8fbd
Mathew Casper <mathew.casper@casperinc.dev>
2022-08-03 13:47:32 +0000

    Add New Test Data

--- ght-service/Service/Resources/40.DML.AddTestData.sql
+++ ght-service/Service/Resources/40.DML.AddTestData.sql
@@ -1,6 +1,7 @@
 -- User Fake User 01
 INSERT INTO public."User" ("UserId", "UserName", "FirstName", "LastName", "Email") VALUES 
-('a335dbb1-84d6-487f-896a-2555c76c8799', 'fake-user-01', 'Fake', 'User', 'fake01@gmail.com');
+ ('a335dbb1-84d6-487f-896a-2555c76c8799', 'fake-user-01', 'Fake', 'User', 'fake01@gmail.com')
+,('0acca7b2-527e-4f34-bd97-d9fc4d74c898','fake-user-02','Fake','User02','fake-user-02@fake.com');
 
 -- Test Campaign 01
 INSERT INTO public."CampaignCampaign" ("Id", "Name", "Description", "GameId", "CreatedBy", "UpdatedBy", "CreatedOnUTC", "UpdatedOnUTC") VALUES
@@ -21,4 +22,34 @@ INSERT INTO public."CampaignCampaignItem" ("ItemId", "CampaignId", "CreatedOnUTC
 
 -- Test Campaign 01 Users
 INSERT INTO public."UserCampaign" ("UserId","CampaignId", "IsOwner") VALUES
- ('a335dbb1-84d6-487f-896a-2555c76c8799','5459c417-c100-407e-b402-e2c087493ec5', TRUE);
+ ('a335dbb1-84d6-487f-896a-2555c76c8799','5459c417-c100-407e-b402-e2c087493ec5', TRUE)
+,('0acca7b2-527e-4f34-bd97-d9fc4d74c898','5459c417-c100-407e-b402-e2c087493ec5', TRUE);
+
+-- Test Combat 01 - Monster Deck
+INSERT INTO public."CombatAttackModifierDecks" ("Id","CreatedBy","UpdatedBy","CreatedOnUTC","UpdatedOnUTC","Positions") VALUES
+ ('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','00000000-0000-0000-0000-000000000000','00000000-0000-0000-0000-000000000000','2022-08-02 22:42:28.200','2022-08-02 22:42:28.200','{}');
+
+-- Test Combat 01 - Monster Deck Cards
+INSERT INTO public."CombatAttackModifierDeckCards" ("DeckId","AttackModifierId","position") VALUES
+  ('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','945471f8-21c5-4e4b-b2ab-a376d7718fb0',0)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','c13d7849-d333-4b21-a2a5-06d7beb9b06b',1)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','9730bd5f-8093-4a47-854c-4e47753e73a3',2)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','d8b7bdac-346e-471c-83b7-9ff2edf5e41a',3)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','03d414d0-e186-48f3-87dc-b109f989e79b',4)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','9730bd5f-8093-4a47-854c-4e47753e73a3',5)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','945471f8-21c5-4e4b-b2ab-a376d7718fb0',6)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','48077f71-7ac2-4298-82cf-ab2c8ce9198f',7)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','c13d7849-d333-4b21-a2a5-06d7beb9b06b',8)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','c13d7849-d333-4b21-a2a5-06d7beb9b06b',9)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','c13d7849-d333-4b21-a2a5-06d7beb9b06b',10)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','d8b7bdac-346e-471c-83b7-9ff2edf5e41a',11)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','428e5822-dd63-4daf-b947-e9835348f955',12)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','945471f8-21c5-4e4b-b2ab-a376d7718fb0',13)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','945471f8-21c5-4e4b-b2ab-a376d7718fb0',14)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','48077f71-7ac2-4298-82cf-ab2c8ce9198f',15)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','48077f71-7ac2-4298-82cf-ab2c8ce9198f',16)
+ ,('360b783c-c83d-4b8f-9aa1-e3ea9d4096bd','48077f71-7ac2-4298-82cf-ab2c8ce9198f',17);
+
+-- Test Combat 01
+INSERT INTO public."CombatCombat" ("Id","CampaignId","ScenarioId","CreatedBy","UpdatedBy","CreatedOnUTC","UpdatedOnUTC","MonsterModifierDeckId","ScenarioLevel") VALUES
+ ('6235d6b1-38dc-4f9d-81cd-7b97b02fd7dd','5459c417-c100-407e-b402-e2c087493ec5','5d0c2238-19de-7aeb-f8db-971289354a38','00000000-0000-0000-0000-000000000000','00000000-0000-0000-0000-000000000000','2022-08-02 22:42:28.200','2022-08-02 22:42:28.200','360b783c-c83d-4b8f-9aa1-e3ea9d4096bd', 2);
