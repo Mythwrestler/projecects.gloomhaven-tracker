267f0b80d03ea23d249124482cf83be4005ea95f
Mathew Casper <mathew.casper@casperinc.dev>
2022-08-31 01:03:06 +0000

    Add Last Seen to Hub Client

---
+++ ght-service/Database/Migrations/20220831010227_add-last-seen-to-hub-client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GloomhavenTracker.Database.Migrations
+{
+    public partial class addlastseentohubclient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastSeen",
+                table: "HubCombatClient",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HubCombatClient_ClientId",
+                table: "HubCombatClient",
+                column: "ClientId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HubCombatClient_ClientId",
+                table: "HubCombatClient");
+
+            migrationBuilder.DropColumn(
+                name: "LastSeen",
+                table: "HubCombatClient");
+        }
+    }
+}
