2b4206dd3b6b4f2db160e588146db27e83abae3e
Mathew Casper <mathew.casper@casperinc.dev>
2022-08-31 01:23:33 +0000

    Prevent simultaneous runs in single instance

--- ght-service/Service/BackgroundServices/CombatHubMontior.cs
+++ ght-service/Service/BackgroundServices/CombatHubMontior.cs
@@ -16,10 +16,10 @@ namespace GloomhavenTracker.Service.BackgroundServices
     public class CombatHubMonitor : IHostedService, IDisposable
     {
         private readonly IHubContext<CombatHub> combatHubContext;
-        private readonly CombatService combatService;
         private readonly IServiceScopeFactory scopeFactory;
         private const int SyncIntervalSeconds = 30;
         private Timer? combatHubClientAuditTimer;
+        private Boolean syncInProgress = false;
 
         public CombatHubMonitor(IHubContext<CombatHub> context, IServiceScopeFactory scopeFactory)
         {
@@ -27,7 +27,7 @@ namespace GloomhavenTracker.Service.BackgroundServices
             this.scopeFactory = scopeFactory;
         }
 
-       public Task StartAsync(CancellationToken stoppingToken)
+        public Task StartAsync(CancellationToken stoppingToken)
         {
             combatHubClientAuditTimer = new Timer(
                 DoWork,
@@ -38,31 +38,40 @@ namespace GloomhavenTracker.Service.BackgroundServices
             return Task.CompletedTask;
         }
 
-        private async void DoWork(object state)
+        private async void DoWork(object? state)
         {
-            using var scope = scopeFactory.CreateScope();
-            CombatService? service = scope.ServiceProvider.GetService<CombatService>();
-            CombatHubClientTracker? clientTracker = scope.ServiceProvider.GetService<CombatHubClientTracker>();
-            if(service is null || clientTracker is null) return;
+            if (syncInProgress) return;
+            syncInProgress = true;
+            try
+            {
+                using var scope = scopeFactory.CreateScope();
+                CombatService? service = scope.ServiceProvider.GetService<CombatService>();
+                CombatHubClientTracker? clientTracker = scope.ServiceProvider.GetService<CombatHubClientTracker>();
+                if (service is null || clientTracker is null) return;
 
-            service.SyncClients();
+                await Task.Run(() => service.SyncClients());
 
-            clientTracker.HubGroups.ForEach(async group => {
-                await combatHubContext.Clients.Group(group).SendAsync(
-                    "ActiveUsers",
-                     new HubRequestResult()
-                     {
-                         data = clientTracker.GetClientsForGroup(group)
-                     }
-                );
-            });
+                var shoutOuts = clientTracker.HubGroups.Select(group =>
+                {
+                    return new Task(async () =>
+                    {
+                        await combatHubContext.Clients.Group(group).SendAsync(
+                         "ActiveUsers",
+                         new HubRequestResult()
+                         {
+                             data = clientTracker.GetClientsForGroup(group)
+                         }
+                     );
+                    });
+                }).ToArray();
 
-            // using var scope = _serviceScopeFactory.CreateScope();
-            // var scopedProcessingService = 
-            //     scope.ServiceProvider.GetRequiredService<IPresenceDatabaseSyncer>();
-
-            // await scopedProcessingService.UpdateConnectionsOnlineStatus()
-        }
+                Task.WaitAll(shoutOuts);
+            }
+            finally
+            {
+                syncInProgress = false;
+            }
+        } 
 
         public Task StopAsync(CancellationToken stoppingToken)
         {
@@ -74,43 +83,5 @@ namespace GloomhavenTracker.Service.BackgroundServices
         {
             combatHubClientAuditTimer?.Dispose();
         }
-
-        // protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        // {
-        //     using var scope = scopeFactory.CreateScope();
-        //     CombatService? service = scope.ServiceProvider.GetService<CombatService>();
-        //     if (service is null) return;
-
-        //     while (!stoppingToken.IsCancellationRequested)
-        //     {
-        //         await combatHubContext.Clients.All.SendAsync("serverMessage", $"Its Been 5 Seconds... Time for another message from the server. messageId: {Guid.NewGuid()}");
-
-        //         await CleanUpRegisteredClients(service);
-
-        //         await Task.Delay(5000);
-        //     }
-        // }
-
-        // public async Task CleanUpRegisteredClients(CombatService service)
-        // {
-        //     var clientsToUpdate = service.CleanUpOldClients();
-        //     clientsToUpdate.Select(kvp => kvp.Key).ToList().ForEach(async combatId =>
-        //     {
-        //         List<User> clients;
-        //         clients = service.GetHubUsersForCombat(combatId).Select(kvp => kvp.Value).ToList();
-        //         await combatHubContext.Clients.Group(combatId.ToString()).SendAsync(
-        //          "UserLeftCombat",
-        //          new HubRequestResult()
-        //          {
-        //              data = clients
-        //          }
-        //     );
-        //     });
-
-        // }
-
-
-
     }
-
 }
